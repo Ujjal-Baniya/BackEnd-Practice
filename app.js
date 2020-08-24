8e12a79fe8022bf6bb291c36a7884e254f6c6348
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-08-24 21:35:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-var events = require("events");
+//events emitter//
+// var events = require("events");
+// var util = require("util")
 
-var myemitter =  new events.EventEmitter();
+// var myemitter =  new events.EventEmitter();
 
-myemitter.on('somevent' , function(mssg){
-    console.log(mssg);
+// myemitter.on('somevent' , function(mssg){
+//     console.log(mssg);
+// })
+
+
+// myemitter.emit('somevent', 'helllo');
+
+// var person = function(name){
+//     this.name = name;
+// };
+
+// util.inherits(person, events.EventEmitter);
+// var james = new person('james');
+// var ram = new person("ram");
+// var harry = new person("harry");
+
+// var people = [james, ram, harry];
+// people.forEach(function(person){
+//     person.on("speak", function(msg){
+//         console.log(person.name + " said "+ msg);
+//     })
+// })
+
+// james.emit("speak", 'hello man');
+// ram.emit("speak", "supp bruh");
+//<------------------------------------------------------------------>
+
+
+//reading and writing
+
+
+var fs = require("fs");
+
+
+
+// // synchronous
+
+// var read = fs.readFileSync("file.txt", 'utf8')
+// fs.writeFileSync("botCreated.txt", read);
+// console.log(read)
+
+// Asynchronous
+
+fs.readFile("file.txt", "utf8", function(err, data){
+    // fs.writeFile("botCreated.txt", data);
+    console.log(data)
 })
-
-
-myemitter.emit('somevent', 'helllo');
