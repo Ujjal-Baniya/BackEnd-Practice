bfe0a13948854e5388130b1152e880efa4e74bf1
Ujjal-Baniya <63017383+Ujjal-Baniya@users.noreply.github.com>
2020-08-26 22:30:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var express = require("express");
 
 var app = express();
 
+app.set('view engine', 'ejs');
 app.listen(3000);                
 
 app.get('/', function(req, res){
-    res.send("Halo!!!")
+    res.send("Halo!!!");
 })
 
 app.get('/contact',function(req, res){
-    res.send("this is contact page")
+    res.send("this is contact page");
 })
 // get = app.get(route, fn)
 // post = app.post(route, fn)
 //  delete = app.delete(route, fn)
+
+
+app.get('/profile/:id', function(req, res){
+    res.render("index", {person :req.params.id});
+})
